3c8d5823703b93c29529e666abc3bb3a0188f31b
Tsagaandari <tsagaanb@bu.edu>
2024-11-05 00:56:24 -0500

    Completed Task2: Authentication: Login and logout

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -8,6 +8,10 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, \
     DeleteView, View
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin 
+from django.contrib.auth.models import User
+from django.contrib.auth import login 
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -19,12 +23,34 @@ class ShowAllProfilesView(ListView):
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
 
+    def get_context_data(self, **kwargs):
+         # Call the base implementation first to get the context
+        context = super().get_context_data(**kwargs)
+        
+        # find the user who is logged in and make sure that they are autenticated
+        if self.request.user.is_authenticated: 
+            profile = Profile.objects.get(user = self.request.user)
+            context['user_profile'] = profile
+
+        return context
+
 
 class ShowProfilePageView(DetailView):
     ''' Show one selected profile '''
     model = Profile
     template_name = 'mini_fb/show_profile.html'
     context_object_name = 'profile' 
+    
+    def get_context_data(self, **kwargs):
+         # Call the base implementation first to get the context
+        context = super().get_context_data(**kwargs)
+        
+        # find the user who is logged in and make sure that they are autenticated
+        if self.request.user.is_authenticated: 
+            profile = Profile.objects.get(user = self.request.user)
+            context['user_profile'] = profile
+
+        return context
 
 class CreateProfileView(CreateView):
     ''' a view to show/process the Create Profile form'''
@@ -34,7 +60,7 @@ class CreateProfileView(CreateView):
     def get_success_url(self):
         ''' displays the Profile model '''
         return self.object.get_absolute_url()
-        
+
     def get_login_url(self) -> str:
         ''' return the URL required for login '''
         return reverse('login')
@@ -48,13 +74,16 @@ class CreateStatusMessageView(LoginRequiredMixin, CreateView):
     def get_context_data(self, **kwargs):
         ''' pass the Profile object to the template '''
         context = super().get_context_data(**kwargs)
-        profile = Profile.objects.get( pk=self.kwargs['pk'])
+
+        # get the logged in user 
+        user = self.request.user
+        profile = Profile.objects.get( user = user)
         context['profile'] = profile
         return context
 
     def form_valid(self, form):
         ''' attach the Profile to the StatusMessage before saving '''
-        profile = Profile.objects.get(pk=self.kwargs['pk'])
+        profile = Profile.objects.get(user=self.request.user)
         form.instance.profile = profile 
         # saves the status message to database
         sm = form.save() 
@@ -71,7 +100,15 @@ class CreateStatusMessageView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         ''' redirect back to the Profile page after successful submission '''
-        return reverse('show_profile', kwargs=self.kwargs)
+        profile = Profile.objects.get(user=self.request.user)
+        return reverse('show_profile', kwargs={'pk': profile.pk})
+        
+    def get_object(self):
+        ''' find the profile related to the USER '''
+        user = self.request.user
+        profile = Profile.objects.get(user = user)
+        
+        return profile
 
 class UpdateProfileView(LoginRequiredMixin, UpdateView):
     ''' a view to process the UpdateProfile form '''
@@ -82,6 +119,13 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
     
     form_class = UpdateProfileForm
     template_name = "mini_fb/update_profile_form.html"
+
+    def get_object(self):
+        ''' find the profile related to the USER '''
+        user = self.request.user
+        profile = Profile.objects.get(user = user)
+        
+        return profile
 
     
 class DeleteStatusMessageView(LoginRequiredMixin, DeleteView):
@@ -112,18 +156,28 @@ class CreateFriendView(LoginRequiredMixin, View):
 
     def dispatch(self, request, *args, **kwargs):
         # Retrieve the Profiles from the URL parameters
-        pk = self.kwargs.get('pk')
+
+        # get the logged in user
+        user = self.request.user
+        # get the profile of the logged in user
+        profile = Profile.objects.get(user = user)
+
         other_pk = self.kwargs.get('other_pk')
         
         # Get the profiles based on the primary keys provided
-        profile = Profile.objects.get(pk=pk)
         other_profile = Profile.objects.get(pk=other_pk)
 
         profile.add_friend(other_profile)
 
         # Redirect back to the profile page of the initiating profile
-        return redirect(reverse('show_profile', kwargs={'pk': pk}))
+        return redirect(reverse('show_profile', kwargs={'pk': profile.id}))
 
+    def get_object(self):
+        ''' find the profile related to the USER '''
+        user = self.request.user
+        profile = Profile.objects.get(user = user)
+        
+        return profile
 
 class ShowFriendSuggestionsView(LoginRequiredMixin, DetailView):
     ''' A view to show all the friend suggestions '''
@@ -131,9 +185,34 @@ class ShowFriendSuggestionsView(LoginRequiredMixin, DetailView):
     model = Profile
     template_name = "mini_fb/friend_suggestions.html"
     context_object_name = "profile"
+
+    def get_object(self):
+        ''' find the profile related to the USER '''
+        user = self.request.user
+        profile = Profile.objects.get(user = user)
+        
+        return profile
     
 class ShowNewsFeedView(LoginRequiredMixin, DetailView):
     ''' A view to show the newsfeed for a Profile '''
     model = Profile
     template_name = "mini_fb/news_feed.html"
     context_object_name = "profile"
+
+    def get_object(self):
+        ''' find the profile related to the USER '''
+        user = self.request.user
+        profile = Profile.objects.get(user = user)
+        
+        return profile
+
+    def get_context_data(self, **kwargs):
+         # Call the base implementation first to get the context
+        context = super().get_context_data(**kwargs)
+        
+        # find the user who is logged in and make sure that they are autenticated
+        if self.request.user.is_authenticated: 
+            profile = Profile.objects.get(user = self.request.user)
+            context['user_profile'] = profile
+
+        return context
