eba5f54fe03eeb21e8fea9fa67a4e2c7ed91b7e8
Tsagaandari <tsagaanb@bu.edu>
2024-11-04 23:16:16 -0500

    Task 2.1: adding url paths to login and logout

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -4,6 +4,7 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from django.contrib.auth import views as auth_views
 
 # create a list of URLs for this app:
 urlpatterns = [
@@ -18,6 +19,8 @@ urlpatterns = [
     path(r'profile/<int:pk>/add_friend/<int:other_pk>/', views.CreateFriendView.as_view(), name='add_friend'),
     path(r'profile/<int:pk>/friend_suggestions', views.ShowFriendSuggestionsView.as_view(), name='friend_suggestions'),
     path(r'profile/<int:pk>/news_feed', views.ShowNewsFeedView.as_view(), name='news_feed'),
+    path(r'login/', auth_views.LoginView.as_view(template_name='mini_fb/login.html'), name='login'),
+    path(r'logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 
 ]
